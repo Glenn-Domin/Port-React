82455d955e061913de9be0206ad5dac69ab9699b
Glenn-Domin <xaduurv@gmail.com>
2017-09-18 21:35:44 +1000

    Adding checkboxes in to reactform

--- src/components/reactform.js
+++ src/components/reactform.js
@@ -1,20 +1,5 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
 import firebase from '../actions/firebase';
-
-//const reactFormContainer = document.querySelector('.react-form-container');
-
-/*class ReactFormLabel extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return(
-      <label htmlFor={this.props.htmlFor}>{this.props.title}</label>
-    )
-  }
-}*/
 
 class ReactForm extends React.Component {
   constructor() {
@@ -25,6 +10,9 @@ class ReactForm extends React.Component {
       agency: '',
       email: '',
       message: '',
+      ask: '',
+      meet: '',
+      hire: '',
       submitted: ''
     }
   }
@@ -32,14 +20,18 @@ class ReactForm extends React.Component {
   handleChange = (e) => {
     let newState = {};
 
-    newState[e.target.name] = e.target.value;
+    if(e.target.type == "checkbox") {
+      newState[e.target.name] = e.target.checked;
+    } else {
+      newState[e.target.name] = e.target.value;
+    }
+
+//    console.log(e.target.type == "checkbox");
 
     this.setState(newState);
 
     this.handleSubmit = this.handleSubmit.bind(this);
   };
-
-
 
   getInitialState() {
     return {'submitted': false};
@@ -52,6 +44,9 @@ class ReactForm extends React.Component {
       formSender: this.state.name,
       formEmail: this.state.email,
       formAgency: this.state.agency,
+      formAsk: this.state.ask,
+      formMeet: this.state.meet,
+      formHire: this.state.hire,
       formMessage: this.state.message
     }
     itemsRef.push(item);
@@ -61,6 +56,9 @@ class ReactForm extends React.Component {
       email: '',
       agency: '',
       message: '',
+      ask: '',
+      meet: '',
+      hire: '',
       submitted: <div className="col-12 mt-3"><div className="alert alert-success" role="alert"><strong>Thanks!</strong> I'll get back to you shortly.</div></div>
     });
   }
@@ -91,6 +89,34 @@ class ReactForm extends React.Component {
           <fieldset className='form-group'>
             <textarea id='formMessage' placeholder='Message' className='form-control' name='message' required onChange={this.handleChange}></textarea>
           </fieldset>
+        </div>
+
+        <div className='col-12 col-md-6'>
+          <div className='form-group'>
+
+
+            <div className="gcore-line-td" id="ftd-wantto">
+
+              <label htmlFor="wantto" className="gcore-label-left">I want to</label>
+              <div className="gcore-display-table gcore-input" id="fin-wantto">
+                <div className="gcore-single-column" id="fclmn">
+                  <div className="gcore-checkbox-item" id="fitem">
+                    <input name="ask" id="ask" type="checkbox" value="Ask" onChange={this.handleChange} />
+                    <label className="gcore-label" htmlFor="ask"><span></span>Ask you a question</label>
+                  </div>
+                  <div className="gcore-checkbox-item" id="fitem1">
+                    <input name="meet" id="meet" type="checkbox" value="Meet" onChange={this.handleChange} />
+                    <label className="gcore-label" htmlFor="meet"><span></span>Meet you</label>
+                  </div>
+                  <div className="gcore-checkbox-item" id="fitem2">
+                    <input name="hire" id="hire" type="checkbox" value="Hire" onChange={this.handleChange} />
+                    <label className="gcore-label" htmlFor="hire"><span></span>Hire you</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
 
         <div className='col-12 col-md-6'>
